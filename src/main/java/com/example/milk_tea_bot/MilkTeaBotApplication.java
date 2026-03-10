6a0f608cbbf3f83be2eb20c20cf2c2144c5ccc7b
Trần Tiến Đạt <ttdat260204@gmail.com>
2026-03-10 17:41:37 +0700

    Feat(Initial): Initial project

---
+++ src/main/java/com/example/milk_tea_bot/MilkTeaBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.milk_tea_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkTeaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkTeaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/milk_tea_bot/MilkTeaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milk_tea_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkTeaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
